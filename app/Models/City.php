084d7575da91686f1b1edae43fe01ce94e7c4670
3omarbadr <omarbadr167@gmail.com>
2022-09-17 15:50:12 +0200

    create database migrations and models

---
+++ app/Models/City.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'country_id'];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function areas()
+    {
+        return $this->hasMany(Area::class);
+    }
+}
